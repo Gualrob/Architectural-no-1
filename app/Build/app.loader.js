645c662a9e93fe5b303d374e67519873b2fa4901
Gualrob <erkus@live.no>
2026-08-26 04:18:52 +0200

    Enda en ny forbedret zoom

--- app/Build/app.loader.js
+++ app/Build/app.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68981537,
+    wasmFileSize: 68981264,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
