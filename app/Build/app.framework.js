86185eebac1919b2a904898b6f27526849f1f04f
Gualrob <erkus@live.no>
2026-08-27 18:41:15 +0200

    Ny

--- app/Build/app.framework.js
+++ app/Build/app.framework.js
@@ -1335,10 +1335,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  5508932: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 5508987: ($0) => { performance.now = function() { return $0; }; },  
- 5509035: ($0) => { performance.now = function() { return $0; }; },  
- 5509083: () => { performance.now = Module['emscripten_get_now_backup']; }
+  5509172: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 5509227: ($0) => { performance.now = function() { return $0; }; },  
+ 5509275: ($0) => { performance.now = function() { return $0; }; },  
+ 5509323: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- app/Build/app.loader.js
+++ app/Build/app.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68981264,
+    wasmFileSize: 68983799,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
